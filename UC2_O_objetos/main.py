b60bb670d1cf614b6d970cea8f1e32eb67595c01
Naomi Hisano <eduardanaomihisano@gmail.com>
2024-11-26 17:55:21 -0300

    26_11_24
    class pilares

---
+++ UC2_O_objetos/main.py
@@ -0,0 +1,20 @@
+
+from conta import conta
+
+
+while True:
+    tipo=int(input("1 - cadastro"))
+
+
+    if tipo == 1:
+        nome = input("Digite seu nome: ")
+        
+
+
+        x = conta(nome, 123,0,123)
+
+
+      
+          
+
+

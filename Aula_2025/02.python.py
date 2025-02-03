07bb80db5ed42879c200ab71963ab339828e00c5
Naomi Hisano <eduardanaomihisano@gmail.com>
2025-02-03 15:32:38 -0300

    aula_2025

---
+++ Aula_2025/02.python.py
@@ -0,0 +1,26 @@
+
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
+from PySide6.QtCore import Qt
+import sys
+
+class HelloWorldWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Olá Mundo")
+        self.setGeometry(150, 80, 300, 100)  
+
+        label = QLabel("Hello world", self)
+        label.setGeometry(0, 0, 300, 100)  
+        label.setAlignment(Qt.AlignCenter) 
+        label.setStyleSheet("font-size: 18px; font-weight: bold;") 
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = HelloWorldWindow()
+    window.show()
+
+    sys.exit(app.exec())
+
+
---
+++ Aula_2025/04.python.py
@@ -0,0 +1,43 @@
+
+import sys
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton
+from PySide6.QtGui import QPixmap
+from PySide6.QtCore import Qt
+
+def on_button_click():
+    print("Botão clicado!")
+
+def main():
+    app = QApplication(sys.argv)
+
+    window = QWidget()
+    window.setWindowTitle('Hello World')
+
+    layout = QVBoxLayout()
+
+    name_label = QLabel('Eduarda Hisano')
+    name_label.setAlignment(Qt.AlignCenter)
+
+    image_label = QLabel(r'C:\Users\suporte\Documents\Code_python\Aula_2025\eduarda.jpg.jpg')
+    pixmap = QPixmap()  
+    image_label.setPixmap(pixmap)
+    image_label.setAlignment(Qt.AlignCenter)
+
+   
+    button = QPushButton('Clique aqui')
+    button.clicked.connect(on_button_click) 
+
+    layout.addWidget(name_label)
+    layout.addWidget(image_label)
+    layout.addWidget(button)
+
+    window.setLayout(layout)
+
+    window.resize(400, 500)
+
+    window.show()
+
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()

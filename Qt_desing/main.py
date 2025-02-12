60a506d09480c854f48a3a49ec8d07efa3341a4e
Naomi Hisano <eduardanaomihisano@gmail.com>
2025-02-12 18:36:40 -0300

    teste

---
+++ Qt_desing/main.py
@@ -0,0 +1,15 @@
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QListView,
+    QMainWindow, QMenu, QMenuBar, QSizePolicy,
+    QStatusBar, QWidget)
+from TelaDeLogin import Ui_MainWindow
+
+if __name__=="__main__":
+    app
